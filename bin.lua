ee71cac8724220da78e9470d2c3aca54a8181399
Christopher Moore <christopher.e.moore@gmail.com>
2024-02-24 16:10:45 +0700

    more

--- bin.lua
+++ bin.lua
@@ -63,7 +63,7 @@ function Bin:normalize()
 	for i=1,self.count do
 		sum = sum + self[i]
 	end
-	if sum == 0 then break end
+	if sum == 0 then return end
 	for i=1,self.count do
 		self[i] = self[i] / sum
 	end
